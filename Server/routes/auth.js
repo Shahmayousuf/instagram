b888ac9ae51f8edee0f3cfe053fb1625804a0ca1
Shahmayousuf <shahmayousuf@gmail.com>
2023-11-21 11:34:59 +0530

    signin and signup routes

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,35 +1,64 @@
-import express  from "express";
-const router=express.Router()
+import express from "express";
+const router = express.Router();
 import mongoose from "mongoose";
-const User = mongoose.model("User")
-router.get('/',(req,res)=>{
-  res.send("hello")
-})
-router.post('/signup',(req,res)=>{
- const {name,email,password}=req.body
- if(!email||!password||!name){
-    return res.status(400).json({error:"please add all the fields"})
- }
-User.findOne({email:email })
-.then((savedUser)=>{
-    if(savedUser){
-        return res.status(400).json({error:"user already exist"})
-    }
-    const user=new User({
-        email,
-        password,
-        name
+const User = mongoose.model("User");
+import bcrypt from "bcrypt";
+
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).json({ error: "please add all the fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(400).json({ error: "user already exist" });
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          password: hashedpassword,
+          name,
+        });
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: "saved successfuly" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     })
-    user.save()
-    .then(user=>{
-        res.json({message:"saved successfuly"})
+    .catch((err) => {
+      console.log(err);
+    });
+});
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "invalid email or password" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.status(422).json({ error: "ivalid email or password" });
+      }
+      bcrypt
+        .compare(password, savedUser.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            res.json({ message: "successfully signed in" });
+          } else {
+            res.json({ error: "inavlid email or password" });
+          }
+        })
+        .cath((err) => {
+          console.log(err);
+        });
     })
-    .catch(err=>{
-        console.log(err);
-    })
-})
-.catch(err=>{
-    console.log(err);
-})
-})
-export default router
+    .catch((err) => {
+      console.log(err);
+    });
+});
+export default router;
